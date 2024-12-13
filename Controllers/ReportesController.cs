4aa8cc2959465c976854f2b75508ab4702df439c
Damian211997 <damjelvez23@cftsa.cl>
2024-12-13 19:21:56 -0300

    reportes agredado

---
+++ Controllers/ReportesController.cs
@@ -0,0 +1,36 @@
+﻿using ExamenProgramacionAvanzada.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamenProgramacionAvanzada.Controllers
+{
+    public class ReportesController : Controller
+    {
+        private readonly ComercialSantaClaraContext _context;
+
+        public ReportesController(ComercialSantaClaraContext context)
+        {
+            _context = context;
+        }
+
+        // Acción para mostrar el reporte de proveedores agrupados por ubicación
+        public async Task<IActionResult> ProveedoresPorUbicacion()
+        {
+            // Agrupar proveedores por ubicación y contar los proveedores en cada ubicación
+            var reporte = await _context.Proveedores
+                .Include(p => p.Ubicacion) // Incluir la información de Ubicación
+                .GroupBy(p => p.Ubicacion.Nombre) // Agrupar por nombre de ubicación
+                .Select(g => new
+                {
+                    Ubicacion = g.Key, // Nombre de la ubicación
+                    NumeroProveedores = g.Count() // Contar el número de proveedores
+                })
+                .ToListAsync();
+
+            // Pasar los resultados a la vista
+            return View(reporte);
+        }
+    }
+}
--- obj/Debug/net8.0/ExamenProgramacionAvanzada.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExamenProgramacionAvanzada.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExamenProgramacionAvanzada")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cf56324d6e7bd3f5702d12c3e20149a3e6b2f95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5b057a232bcd99bdadd52a231c8ab14b69076ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExamenProgramacionAvanzada")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExamenProgramacionAvanzada")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
